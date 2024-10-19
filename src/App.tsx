e67d05145046411a7ac3a3fb660258b77b87e723
hyunho <kimhyunhi1@gmail.com>
2024-10-19 17:18:41 +0900

    78

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import CreateAccount from "./routes/create-account";
+import Login from "./routes/login";
+import { createGlobalStyle } from "styled-components";
+import { useEffect, useState } from "react";
+import LoadingScreen from "./components/loading-screen";
+import reset from 'styled-reset'; 
+
+
+const router =createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout />,
+    children :[
+      {
+      path: "",
+      element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      }
+      
+    ]
+  },
+  {
+    path: "/login",
+    element:<Login />
+  },
+  {path: "/create-account", element:<CreateAccount />}
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body{
+  background-color: black;
+  color: white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+  Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvatica Neue',
+  sans-serif;
+}
+`;
+
+
+function App() {
+  const[isLoading, setLoading] = useState(true);
+  const init = async () => {
+    //waiting for firebase
+    setTimeout(() => setLoading(false), 2000) 
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return(
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} /> }
+    </>
+  );
+}
+
+export default App;
