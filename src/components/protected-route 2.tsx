0f0364194cd099005057a5365ebcca0458f18747
hyunho <kimhyunhi1@gmail.com>
2024-11-29 14:41:03 +0900

    Save progress

---
+++ src/components/protected-route 2.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from 'react-router-dom';
+import { auth } from '/src/firebase';
+export default function ProtectedRoute({
+    children,
+}: {
+    children:React.ReactNode;
+}){
+    const user = auth.currentUser;
+    console.log(user)
+    if(user === null){
+        return <Navigate to="/login" />
+    }
+    return children
+}
